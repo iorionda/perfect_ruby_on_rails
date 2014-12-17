5fb83febecc1bc7a814fa9e88f3c730e6b0b2716
Iori ONDA <iori.onda@gmail.com>
2014-12-17 13:09:37 +0900

    Impl list 2.8

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -9,4 +9,10 @@ class Book < ActiveRecord::Base
   validates :name, presence: true
   validates :name, length: { maximum: 15 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate do |book|
+    if book.name.include?('exercise')
+      book.errors[:name] << 'I do not like exercise'
+    end
+  end
 end
