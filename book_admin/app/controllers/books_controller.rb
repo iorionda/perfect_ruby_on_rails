3711c21a4dfbc601074f38566a3a95aa3d8bec79
Iori ONDA <iori.onda@gmail.com>
2014-12-18 13:16:11 +0900

    Impl list 2.15

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+  def show
+    @book = Book.find(param[:id])
+
+    respond_to do |format|
+      format.html
+      formant.csv
+      format.json
+    end
+  end
 end
