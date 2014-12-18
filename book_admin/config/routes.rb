99c65c984dda6d24a2c206cbef5bf4e72d8bdf12
Iori ONDA <iori.onda@gmail.com>
2014-12-18 13:55:47 +0900

    Chack resources sample
                 Prefix Verb   URI Pattern                                        Controller#Action
        publisher_books GET    /publishers/:publisher_id/books(.:format)          books#index
                        POST   /publishers/:publisher_id/books(.:format)          books#create
     new_publisher_book GET    /publishers/:publisher_id/books/new(.:format)      books#new
    edit_publisher_book GET    /publishers/:publisher_id/books/:id/edit(.:format) books#edit
         publisher_book GET    /publishers/:publisher_id/books/:id(.:format)      books#show
                        PATCH  /publishers/:publisher_id/books/:id(.:format)      books#update
                        PUT    /publishers/:publisher_id/books/:id(.:format)      books#update
                        DELETE /publishers/:publisher_id/books/:id(.:format)      books#destroy
       detail_publisher GET    /publishers/:id/detail(.:format)                   publishers#detail
      search_publishers GET    /publishers/search(.:format)                       publishers#search
             publishers GET    /publishers(.:format)                              publishers#index
                        POST   /publishers(.:format)                              publishers#create
          new_publisher GET    /publishers/new(.:format)                          publishers#new
         edit_publisher GET    /publishers/:id/edit(.:format)                     publishers#edit
              publisher GET    /publishers/:id(.:format)                          publishers#show
                        PATCH  /publishers/:id(.:format)                          publishers#update
                        PUT    /publishers/:id(.:format)                          publishers#update
                        DELETE /publishers/:id(.:format)                          publishers#destroy

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  get "books/:id" => "books#show"
+  # get "books/:id" => "books#show"
+  # resources :publishers
+
+  resources :publishers do
+    resources :books
+
+    member do
+      get 'detail'
+    end
+
+    collection do
+      get 'search'
+    end
+  end
 end
