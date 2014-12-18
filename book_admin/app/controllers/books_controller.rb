97e2eaa49c224e3f91d3ad118d395b2dde720b12
Iori ONDA <iori.onda@gmail.com>
2014-12-18 14:12:56 +0900

    Impl before_action

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_actoin :set_book, only: [:show, :edit, :update, :destroy]
   def show
     @book = Book.find(param[:id])
 
@@ -7,5 +8,11 @@ class BooksController < ApplicationController
       formant.csv
       format.json
     end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(param[:id])
   end
 end
