46578628e2849553bd651738a475eb76ffd956fb
Iori ONDA <iori.onda@gmail.com>
2015-03-05 14:03:06 +0900

    Use jbuilder

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -4,14 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     respond_to do |format|
-      format.html
-      format.csv
+      format.json
     end
-  end
-
-  private
-
-  def set_book
-    @book = Book.find(params[:id])
   end
 end
